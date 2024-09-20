4469ab2e6a42aed58bfd08c55ecc0810c56774bb
Mttewk <mttewk@gmail.com>
2024-09-20 16:48:50 +0300

    Initial Commit

---
+++ STUDY/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  STUDY
+//
+//  Created by Матвей Абрамов on 20.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
