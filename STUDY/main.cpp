9ae836504a5c477e69d978b7ced7aa2828707c8c
Mttewk <mttewk@gmail.com>
2024-09-21 12:06:43 +0300

    мат операция

--- STUDY/main.cpp
+++ STUDY/main.cpp
@@ -1,8 +1,8 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(void) {
+    int a = 2;
+    int b = 3;
+    printf("%d\n", a+b);
 }
